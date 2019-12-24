1b69bd3753a76b7c57a547bad9b44a1036658083
gafderks <geertderks12@gmail.com>
2019-12-24 23:03:19 +0100

    Improved template hierarchy and added login view

--- dbase/settings.py
+++ dbase/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'compressor',
 ]
 
 MIDDLEWARE = [
@@ -129,8 +130,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
--- dbase/urls.py
+++ dbase/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('booking/', include('booking.urls')),
+    path('login/', auth_views.LoginView.as_view()),
+    path('logout/', auth_views.LogoutView.as_view()),
     path('admin/', admin.site.urls),
 ]
 # The following patterns only work if DEBUG = TRUE. Django should not be used for serving static files or uploaded files
