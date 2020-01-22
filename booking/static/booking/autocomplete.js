5863f403f6865ac1754aa99f3458cc8d09398036
gafderks <geertderks12@gmail.com>
2020-01-22 19:36:40 +0100

    Use better ES6 syntax for autocomplete.js

--- booking/static/booking/autocomplete.js
+++ booking/static/booking/autocomplete.js
@@ -1,10 +1,11 @@
-var materialEngine = new Bloodhound({
+/*global Bloodhound */
+const materialEngine = new Bloodhound({
     datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
     queryTokenizer: Bloodhound.tokenizers.whitespace,
     prefetch: materialsUrl,
 });
 
-var materialaliasEngine = new Bloodhound({
+const materialaliasEngine = new Bloodhound({
     datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
     queryTokenizer: Bloodhound.tokenizers.whitespace,
     prefetch: materialAliasesUrl,
@@ -22,23 +23,21 @@ $('.typeahead-materials').typeahead({
         templates: {
             suggestion: function (e) {
                 if (e.material.images.length > 0) {
-                    imgHtml = '<div class="tt-suggestion-image float-right"><img src="' + e.material.images[0] + '"></div>';
+                    imgHtml = `<div class="tt-suggestion-image float-right"><img src="${e.material.images[0]}"></div>`;
                 } else {
-                    imgHtml = '<div class="tt-suggestion-image tt-suggestion-no-image"></div>'
+                    imgHtml = `<div class="tt-suggestion-image tt-suggestion-no-image"></div>`;
                 }
-                return [
-                    '<div class="clearfix">',
-                    imgHtml,
-                    '<div class="tt-suggestion-text">',
-                    '<div class="tt-suggestion-name" data-id="' + e.material.id + '">' + e.name + '</div>',
-                    '<div class="tt-suggestion-alias">' + e.material.name + '</div>',
-                    '<div class="clearfix">',
-                    '<div class="tt-suggestion-category">' + (e.material.categories.length > 0 ? e.material.categories[0] : "") + '</div>',
-                    (e.material.gm === true ? '<div class="tt-suggestion-gm">GM</div>' : ''),
-                    '</div>',
-                    '</div>',
-                    '</div>'
-                ].join('\n');
+                return `<div class="clearfix">
+                            ${imgHtml}
+                            <div class="tt-suggestion-text">
+                                <div class="tt-suggestion-name" data-id="${e.material.id}">${e.name}</div>
+                                <div class="tt-suggestion-alias">${e.material.name}</div>
+                                <div class="clearfix">
+                                    <div class="tt-suggestion-category">${(e.material.categories.length > 0 ? e.material.categories[0] : "")}</div>
+                                    ${(e.material.gm === true ? '<div class="tt-suggestion-gm">GM</div>' : '')}
+                                </div>
+                            </div>
+                        </div>`;
             }
         }
     }
@@ -49,22 +48,20 @@ $('.typeahead-materials').typeahead({
         templates: {
             suggestion: function (e) {
                 if (e.images.length > 0) {
-                    imgHtml = '<div class="tt-suggestion-image float-right"><img src="' + e.images[0] + '"></div>';
+                    imgHtml = `<div class="tt-suggestion-image float-right"><img src="${e.images[0]}"></div>`;
                 } else {
-                    imgHtml = '<div class="tt-suggestion-image tt-suggestion-no-image"></div>'
+                    imgHtml = `<div class="tt-suggestion-image tt-suggestion-no-image"></div>`;
                 }
-                return [
-                    '<div class="clearfix">',
-                    imgHtml,
-                    '<div class="tt-suggestion-text">',
-                    '<div class="tt-suggestion-name" data-id="' + e.id + '">' + e.name + '</div>',
-                    '<div class="clearfix">',
-                    '<div class="tt-suggestion-category">' + (e.categories.length > 0 ? e.categories[0] : "") + '</div>',
-                    (e.gm === true ? '<div class="tt-suggestion-gm">GM</div>' : ''),
-                    '</div>',
-                    '</div>',
-                    '</div>'
-                ].join('\n');
+                return `<div class="clearfix">
+                            ${imgHtml}
+                            <div class="tt-suggestion-text">
+                                <div class="tt-suggestion-name" data-id="${e.id}">${e.name}</div>
+                                <div class="clearfix">
+                                    <div class="tt-suggestion-category">${(e.categories.length > 0 ? e.categories[0] : "")}</div>
+                                    ${(e.gm === true ? '<div class="tt-suggestion-gm">GM</div>' : '')}
+                                </div>
+                            </div>
+                        </div>`;
             }
         }
     });
