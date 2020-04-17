3cfaa50d6d07c08ff4c2f7195c6d14945d2d49fe
gafderks <geertderks12@gmail.com>
2020-04-18 00:25:36 +0200

    Use merge for merging streams in gulp tasks and implemented image pipeline. (Fixes #26)

--- dbase/static/dbase/config.gulp.js
+++ dbase/static/dbase/config.gulp.js
@@ -11,5 +11,9 @@ export default {
   styles: {
     src: './dbase/static/dbase/css/app.scss',
     dest: './build/dbase/css/'
+  },
+  images: {
+    src: './dbase/static/dbase/img/**/*',
+    dest: './build/dbase/img/'
   }
 };
