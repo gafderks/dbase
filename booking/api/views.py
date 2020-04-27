ac92b3db28e9ef4e1a3ba90ea8bd35fa15c3b7c8
gafderks <geertderks12@gmail.com>
2020-04-27 13:21:14 +0200

    Prefetch categories and material images for material API for performance improvement

--- booking/api/views.py
+++ booking/api/views.py
@@ -5,24 +5,25 @@ from booking.models import Material, MaterialAlias
 
 
 def export_material(request):
-    materials = Material.objects.all()
 
-    format = request.GET.get("format", "json")
+    materials = Material.objects.prefetch_related("categories", "images").all()
+    export_format = request.GET.get("format", "json")
 
-    if format == "woocommerce":
+    if export_format == "woocommerce":
         return material.format_woocommerce(request, materials)
-    elif format == "json":
+    elif export_format == "json":
         return material.format_json(request, materials)
     else:
         return HttpResponseBadRequest("Unknown format")
 
 
 def export_materialalias(request):
+
     material_aliases = MaterialAlias.objects.all()
 
-    format = request.GET.get("format", "json")
+    export_format = request.GET.get("format", "json")
 
-    if format == "json":
+    if export_format == "json":
         return materialalias.format_json(request, material_aliases)
     else:
         return HttpResponseBadRequest("Unknown format")
