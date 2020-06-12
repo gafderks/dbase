76f444ecf14f2a46c447146a51ccc705156fa5d1
gafderks <geertderks12@gmail.com>
2020-06-12 19:37:08 +0200

    Added initial tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class LoginPageTest(TestCase):
+    def test_redirect_to_login_page(self):
+        requested_page = "/"
+        response = self.client.get(requested_page)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response["location"], "/users/login/?next=" + requested_page)
+
+    def test_uses_login_template(self):
+        response = self.client.get("/users/login/")
+        self.assertTemplateUsed(response, "registration/login.html")
