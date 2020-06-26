2a77da76563167ca6f90440ea90af977cb16d6e1
gafderks <geertderks12@gmail.com>
2020-06-26 22:13:08 +0200

    Put back the dependency but then on the latest migration

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = []
+    dependencies = [("auth", "__latest__")]
 
     operations = [
         migrations.CreateModel(
