b085aecd9eecc877dedde4c9b43d9ee469cd72e2
gafderks <geertderks12@gmail.com>
2020-06-28 21:25:17 +0200

    Debugging statements in FT

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -10,6 +10,7 @@ MAX_WAIT = 10
 
 def wait(fn):
     def modified_fn(*args, **kwargs):
+        print("run wait")
         start_time = time.time()
         while True:
             try:
@@ -58,6 +59,7 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     @wait
     def wait_to_be_logged_in(self, user_label):
+        print(self.browser.page_source)
         self.browser.find_element_by_link_text("Logout")
         navbar = self.browser.find_element_by_css_selector(".navbar")
         self.assertIn(user_label, navbar.text)
