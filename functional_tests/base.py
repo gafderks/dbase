ee44db34f111da9090ebfa8d03976b27c3589434
gafderks <geertderks12@gmail.com>
2020-06-28 22:33:35 +0200

    Revert "Debugging statements in FT"
    This reverts commit b085aecd9eecc877dedde4c9b43d9ee469cd72e2.

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -10,7 +10,6 @@ MAX_WAIT = 10
 
 def wait(fn):
     def modified_fn(*args, **kwargs):
-        print("run wait")
         start_time = time.time()
         while True:
             try:
@@ -59,7 +58,6 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     @wait
     def wait_to_be_logged_in(self, user_label):
-        print(self.browser.page_source)
         self.browser.find_element_by_css_selector('[href="/users/logout/"]')
         navbar = self.browser.find_element_by_css_selector(".navbar")
         self.assertIn(user_label, navbar.text)
