91acb7138d4df076a59e24d75cae1a5b39be7353
gafderks <geertderks12@gmail.com>
2020-08-08 17:15:40 +0200

    Fix migration

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [("auth", "__latest__")]
+    dependencies = [("auth", "0001_initial")]
 
     operations = [
         migrations.CreateModel(
