497394da368a13045060a3d22f22f9b29cc854fb
gafderks <geertderks12@gmail.com>
2020-09-30 08:54:27 +0200

    Ease the SQLite by creating 10 materials in SimpleBUserBookingTest instead of 50 to fix OperationalError "database table locked"

--- functional_tests/test_create_booking.py
+++ functional_tests/test_create_booking.py
@@ -1,76 +1,12 @@
-import time
 from datetime import timedelta
 
 from django.contrib.auth.models import Permission
-from django.core.management import call_command
-from django.utils import timezone
 
-from booking.models import Event, PartOfDay
+from booking.models import PartOfDay
 from booking.tests.factories import EventFactory, MaterialFactory
-from users.models import Group
 from users.tests.factories import UserFactory, RoleFactory
 from .base import FunctionalTest, english
-
 from .game_view_page import GameViewPage
-
-
-# def import_initial_data():
-#     call_command("importmaterial")
-#     call_command("creategroups")
-#     call_command("importfilters")
-
-
-# def create_events():
-#     def now_plus(days_delta):
-#         return timezone.now() + timedelta(days=days_delta)
-#
-#     Event(
-#         name="Active event",
-#         slug="active-event",
-#         booking_start=now_plus(-7),
-#         booking_end=now_plus(1),
-#         privileged_booking_end=now_plus(1),
-#         event_start=now_plus(72),
-#         event_end=now_plus(82),
-#     ).save()
-#     Event(
-#         name="Privileged event",
-#         slug="privileged-event",
-#         booking_start=now_plus(-3),
-#         booking_end=now_plus(-1),
-#         privileged_booking_end=now_plus(1),
-#         event_start=now_plus(3),
-#         event_end=now_plus(6),
-#     ).save()
-#     Event(
-#         name="Locked event",
-#         slug="locked-event",
-#         booking_start=now_plus(-7),
-#         booking_end=now_plus(1),
-#         privileged_booking_end=now_plus(1),
-#         event_start=now_plus(20),
-#         event_end=now_plus(25),
-#         locked=True,
-#     ).save()
-#     Event(
-#         name="Locked event 2",
-#         slug="locked-event-2",
-#         booking_start=now_plus(-12),
-#         booking_end=now_plus(-7),
-#         privileged_booking_end=now_plus(-8),
-#         event_start=now_plus(55),
-#         event_end=now_plus(60),
-#     ).save()
-#     Event(
-#         name="Hidden event",
-#         slug="hidden-event",
-#         booking_start=now_plus(-7),
-#         booking_end=now_plus(1),
-#         privileged_booking_end=now_plus(1),
-#         event_start=now_plus(20),
-#         event_end=now_plus(25),
-#         visible=False,
-#     ).save()
 
 
 @english
@@ -105,7 +41,7 @@ class SimpleUserBookingTest(FunctionalTest):
 
         self.active_event = EventFactory()
 
-        self.materials = MaterialFactory.create_batch(size=50)
+        self.materials = MaterialFactory.create_batch(size=10)
         MaterialFactory(name="beschuiten (rol)")
         MaterialFactory(name="EHBO doos")
 
