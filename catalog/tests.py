2a145ebed6e98bff68dfd854b815a2af6d2dd7a6
gafderks <geertderks12@gmail.com>
2020-10-13 12:44:05 +0200

    Added button to material detail view for editing the material

--- catalog/tests.py
+++ catalog/tests.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from django.conf import settings
 from django.test import TestCase
 from django.urls import reverse
@@ -42,3 +44,25 @@ class MaterialDetailViewTest(TestCase):
         response = self.client.get(lendable_material.get_absolute_url())
         self.assertTemplateUsed(response, "catalog/material_detail.html")
         self.assertContains(response, lendable_material.get_shop_url())
+
+    @patch("django.contrib.auth.backends.ModelBackend.has_perm")
+    def test_material_edit_link(self, mock_has_perm):
+        mock_has_perm.return_value = True
+        material = MaterialFactory()
+        user = UserFactory()
+        self.client.force_login(user)
+        response = self.client.get(material.get_absolute_url())
+        self.assertTemplateUsed(response, "catalog/material_detail.html")
+        self.assertContains(
+            response, reverse("admin:booking_material_change", args=[material.pk])
+        )
+        mock_has_perm.assert_any_call(user, "booking.change_material", None)
+
+    def test_material_edit_link_no_perm(self):
+        material = MaterialFactory()
+        self.client.force_login(UserFactory())
+        response = self.client.get(material.get_absolute_url())
+        self.assertTemplateUsed(response, "catalog/material_detail.html")
+        self.assertNotContains(
+            response, reverse("admin:booking_material_change", args=[material.pk])
+        )
