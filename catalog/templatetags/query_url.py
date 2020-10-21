8b5fac55ad5992143c26fca1959917e4ba534eb0
gafderks <geertderks12@gmail.com>
2020-10-21 23:45:42 +0200

    Created tests for param_replace template tag

--- catalog/templatetags/query_url.py
+++ catalog/templatetags/query_url.py
@@ -7,7 +7,7 @@ register = template.Library()
 def param_replace(context, **kwargs):
     """
     Replace GET parameters in the current URL while leaving the other parameters intact.
-    Setting a parameter falsey removes it from the query string.
+    Setting a parameter None removes it from the query string.
 
     :param dict context: request context
     :param dict kwargs: key, value dict
@@ -17,7 +17,7 @@ def param_replace(context, **kwargs):
     # Set variables for which a value is given
     for key, value in kwargs.items():
         get_variable_dict[key] = value
-    # Remove variables for which the value is falsey
-    for key in [key for key, value in get_variable_dict.items() if not value]:
+    # Remove variables for which the value is None
+    for key in [key for key, value in get_variable_dict.items() if value is None]:
         del get_variable_dict[key]
     return get_variable_dict.urlencode()
