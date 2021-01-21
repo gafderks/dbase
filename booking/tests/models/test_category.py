9883f8db5364f548bcf6f7c5a9fcff29ab3aa983
gafderks <geertderks12@gmail.com>
2021-01-21 23:07:19 +0100

    Added test for category ancestors

--- booking/tests/models/test_category.py
+++ booking/tests/models/test_category.py
@@ -4,7 +4,7 @@ from booking.models import Material, Category
 from booking.tests.factories import MaterialFactory, CategoryFactory
 
 
-class RateClassModelTest(TestCase):
+class CategoryModelTest(TestCase):
     def test_delete_category_keeps_materials(self):
         category = CategoryFactory()
         material = MaterialFactory()
@@ -12,3 +12,9 @@ class RateClassModelTest(TestCase):
         category.delete()
         self.assertEqual(Material.objects.count(), 1)
         self.assertEqual(Category.objects.count(), 0)
+
+    def test_category_tree(self):
+        base = CategoryFactory()
+        child = CategoryFactory(parent=base)
+        grandchild = CategoryFactory(parent=child)
+        self.assertTrue(base in grandchild.get_ancestors())
