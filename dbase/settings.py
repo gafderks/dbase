acec7d0b6bc44a9e3c1d940806e15dd389511021
gafderks <geertderks12@gmail.com>
2023-01-15 23:10:35 +0100

    Test if CI variable is set and if so use dummy caching

--- dbase/settings.py
+++ dbase/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False),
+    CI=(bool, False),
     DEBUG_TOOLBAR=(bool, False),
     ALLOWED_HOSTS=(list, ["127.0.0.1", "localhost"]),
     DATA_UPLOAD_MAX_MEMORY_SIZE=(int, 10485760),  # For uploading HD images
@@ -158,7 +159,7 @@ CACHES = {
         "LOCATION": env("MEMCACHE_LOCATIONS"),
     }
 }
-if DEBUG:
+if DEBUG or env("CI"):
     CACHES = {
         "default": {
             "BACKEND": "django.core.cache.backends.dummy.DummyCache",
